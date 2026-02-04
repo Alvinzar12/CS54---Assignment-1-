f366a2eafe06c7b16585c14141974e7e8291d964
Alvin T <alvino32143@gmail.com>
2026-02-04 00:30:48 -0500

    completed version2

--- assignment01/Version2.java
+++ assignment01/Version2.java
@@ -1,10 +1,49 @@
 package assignment01;
 
+import java.util.*;
+
 public class Version2 implements Code {
+
+	private Map<Character, Integer> encodeMap;
+    private Map<Integer, Character> decodeMap;
+    private Random random;
 
 	public Version2() {
 
-	
+		encodeMap = new TreeMap<>();
+        decodeMap = new TreeMap<>();
+        random = new Random();
+
+        List<Character> chars = new ArrayList<>();
+
+        for (char c = 'A'; c <= 'Z'; c++) {
+            chars.add(c);
+        }
+
+        chars.add(' ');
+        chars.add(',');
+        chars.add('.');
+        chars.add(':');
+        chars.add(';');
+        chars.add('?');
+        chars.add('!');
+
+        List<Integer> codes = new ArrayList<>();
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                codes.add(i * 10 + j);
+            }
+        }
+
+        Collections.shuffle(codes, random);
+
+        for (int i = 0; i < chars.size(); i++) {
+            char ch = chars.get(i);
+            int code = codes.get(i);
+
+            encodeMap.put(ch, code);
+            decodeMap.put(code, ch);
+        }
 	
 	}
 	
@@ -12,6 +51,24 @@ public class Version2 implements Code {
 	public String encode(String input) {
 		String output = "";
 
+		input = input.toUpperCase(); 
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (!encodeMap.containsKey(ch)) {
+                continue; 
+            }
+
+            output += encodeMap.get(ch);
+
+            output += (8 + random.nextInt(2));
+            output += (8 + random.nextInt(2));
+
+            if (random.nextInt(10) > 7) {
+                output += "0";
+            }
+        }
 		
 		return output;
 	}
@@ -19,8 +76,26 @@ public class Version2 implements Code {
 	@Override
 	public String decode(String input) {
 		String output = "";
+		int i = 0;
 
-		
-		return output;
+        while (i < input.length() - 1) {
+            char c = input.charAt(i);
+
+            if (c == '8' || c == '9' || c == '0') {
+                i++;
+                continue;
+            }
+
+            String value = "" + input.charAt(i) + input.charAt(i + 1);
+            int code = Integer.parseInt(value);
+
+            if (decodeMap.containsKey(code)) {
+                output += decodeMap.get(code);
+            }
+
+            i += 2;
+        }
+
+		return output.toLowerCase();
 	}
 }
